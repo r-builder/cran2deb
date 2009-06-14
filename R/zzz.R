f5c169f8ae40aedc41dca4b9085d56587308d826
Charles Blundell <blundellc@gmail.com>
2009-06-14 15:54:24 +0000

    email acct

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,7 @@
         stop('Invalid system specification: must be of the form name-arch')
     }
     global("host_arch", gsub('^[a-z]+-','',which_system))
-    global("maintainer", 'cran2deb autobuild <cran2deb@example.org>')
+    global("maintainer", 'cran2deb autobuild <cran2deb@gmail.com>')
     global("root", system.file(package='cran2deb'))
     global("cache_root", '/var/cache/cran2deb')
     global("pbuilder_results",  file.path('/var/cache/cran2deb/results',which_system))
