f817413f47988b6ce59586458d1dc73ef4589cb9
Alexander Mohr <thehesiod@hotmail.com>
2019-03-28 21:58:50 -0700

    tweaks

--- build-package.py
+++ build-package.py
@@ -121,7 +121,7 @@ class PackageBuilder:
             assert m, f"Unknown line: {line}, with cran_name: {r_cran_name}"
 
             m = m.groupdict()
-            if m['deptype'] in {"Suggests", "Conflicts", "Recommends"}:
+            if m['deptype'] in {"Suggests", "Conflicts", "Recommends", "Replaces"}:
                 continue
 
             assert m['deptype'] == "Depends", f"Unknown deptype for line: {line}"
@@ -273,7 +273,7 @@ def main():
     app_args.cran_pkg_name = app_args.cran_pkg_name[0]
 
     # TODO: this does work for deb builder, see if fix in python works
-    os.environ['MAKE'] = f"make -j {_num_cpus}"
+    os.environ["DEB_BUILD_OPTIONS"] = f'parallel={_num_cpus}'
 
     if not os.path.exists(_dist_path):
         with open(_dist_path, "w") as f:
