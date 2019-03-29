ec7395c23acca4b73acf08922fa41a5577c915bc
Alexander Mohr <thehesiod@hotmail.com>
2019-03-28 21:49:47 -0700

    bugfix

--- build-package.py
+++ build-package.py
@@ -90,7 +90,7 @@ class HttpDebRepo:
         if self._deb_info is None:
             self.refresh()
 
-        pkg_name = f"r-cran-{pkg_name}"
+        pkg_name = f"r-cran-{pkg_name}".lower()
         deb_ver = _get_deb_version(deb_ver)
         return deb_ver in self._deb_info.get(pkg_name, _empty_dict)
 
